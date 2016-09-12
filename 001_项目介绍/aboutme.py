a3c0f19e8ddafc8e7c04bc74567a3fd1087b97f4
Guibs82 <805249506@qq.com>
2016-09-12 13:15:13 +0800

    项目介绍
    项目介绍

---
+++ 001_项目介绍/aboutme.py
@@ -0,0 +1,7 @@
+# 个人及项目介绍
+print("---------------------")
+print("Author: Guibs[桂布斯]")
+print("Book: 《精通 Python 设计模式》")
+print("GitHub: https://github.com/Guibs82")
+print("Python: 3.5.2")
+print("---------------------")
